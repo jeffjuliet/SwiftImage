43d5106fd5c7a0bfbe91618e07810a05e12d1d09
jefffyangdis <fangyang@fangyangdemacbook-pro.local>
2017-06-06 08:49:02 +0800

    Initial Commit

---
+++ SwiftImage/SwiftImage.h
@@ -0,0 +1,19 @@
+//
+//  SwiftImage.h
+//  SwiftImage
+//
+//  Created by 方阳 on 2017/6/6.
+//  Copyright © 2017年 jefffyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftImage.
+FOUNDATION_EXPORT double SwiftImageVersionNumber;
+
+//! Project version string for SwiftImage.
+FOUNDATION_EXPORT const unsigned char SwiftImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftImage/PublicHeader.h>
+
+
